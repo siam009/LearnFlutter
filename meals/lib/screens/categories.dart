3a397d099d9fbd825d2529c3034056ff4c4b98e7
Arnab Ahamed Siam <arnab.siam@brainstation-23.com>
2025-03-07 22:31:27 +0600

    tabbar implemented

--- meals/lib/screens/categories.dart
+++ meals/lib/screens/categories.dart
@@ -22,9 +22,6 @@ class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("pick your category"),
-      ),
       body: GridView(
         padding: const EdgeInsets.all(24),
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
