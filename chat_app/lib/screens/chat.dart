951c10dc0cc22e6c5552f49f9886e624c2d14e80
Arnab Ahamed Siam <arnab.siam@brainstation-23.com>
2025-04-20 22:53:44 +0600

    creates chat meesage ui and sending data to firestore

---
+++ chat_app/lib/screens/chat.dart
@@ -0,0 +1,37 @@
+import 'package:chat_app/main.dart';
+import 'package:chat_app/widget/chat_messages.dart';
+import 'package:chat_app/widget/new_message.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("FlutterChat"),
+        actions: [
+          IconButton(
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+            icon: Icon(
+              Icons.exit_to_app,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+          )
+        ],
+      ),
+      body: Column(
+        children: const [
+          Expanded(
+            child: ChatMessages(),
+          ),
+          NewMessage(),
+        ],
+      ),
+    );
+  }
+}
